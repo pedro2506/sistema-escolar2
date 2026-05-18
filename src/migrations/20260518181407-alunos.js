b6c826ec1995a56a74f9fd0e6d0cfa322f60f601
pedro2506 <t3pedropaulo@gmail.com>
2026-05-18 15:20:48 -0300

    up7

---
+++ src/migrations/20260518181407-alunos.js
@@ -0,0 +1,41 @@
+'use strict';
+const CreatedAndUpdated = require('../models/CreatedAndUpdated');
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Alunos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(20)
+        allowNull: false
+      },
+      surname: {
+        allowNull: false
+        type: Sequelize.STRING(20)
+      },
+      email: {
+        type: Sequelize.STRING(50)
+        allowNull: false
+      },
+      document: {
+        allowNull: false,
+        type: Sequelize.STRING(20)
+      },
+      document_type: {
+        allowNull: false,
+        type: Sequelize.STRING(20)
+      },
+      ...CreatedAndUpdated
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Alunos');
+  }
+};
